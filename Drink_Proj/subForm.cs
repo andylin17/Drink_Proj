d476dcf23dffd692cd0f5f6d45da9f441e1f40bd
akka2019- <edcd9860@gmail.com>
2019-09-10 21:40:48 +0800

    補

---
+++ Drink_Proj/subForm.cs
@@ -0,0 +1,40 @@
+﻿using Common;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Drink_Proj
+{
+    public partial class subForm : BaseForm
+    {
+        public subForm(DrinkData drink)
+        {
+            InitializeComponent();
+            Bind();
+            confirm.DialogResult = DialogResult.OK;
+            this.drink = drink;
+        }
+        //傳進來的飲料物件
+        DrinkData drink; 
+        //配料物件
+        private BtnData stuffData;
+
+        public override void Button_Click(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            stuffData = btnDatas.Where(x => x.Text == btn.Text).SingleOrDefault();
+        }
+        //關掉視窗時，存ListViewItem 字串[]
+        private void confirm_Click(object sender, EventArgs e)
+        {
+            drink.stuffname = stuffData?.Text ?? "";
+            drink.stuffprice = stuffData?.Price ?? 0;
+        }
+    }
+}
